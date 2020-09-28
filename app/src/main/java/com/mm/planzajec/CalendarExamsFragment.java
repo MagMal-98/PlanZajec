6b871f9f432edae146d21d8092ae722dda89d701
magmal <magdalena.malczewska@yahoo.pl>
2020-09-28 14:24:32 +0200

    working navigation panel

---
+++ app/src/main/java/com/mm/planzajec/CalendarExamsFragment.java
@@ -0,0 +1,21 @@
+package com.mm.planzajec;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CalendarExamsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calendar_exams, container, false);
+    }
+}
