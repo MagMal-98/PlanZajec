7257490c86538ab482351bf4ffe9eea05da26922
magmal <magdalena.malczewska@yahoo.pl>
2020-09-29 13:16:35 +0200

    working navigation on activities working tab layout for daily schedule

--- app/src/main/java/com/mm/planzajec/MainActivity.java
+++ app/src/main/java/com/mm/planzajec/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 //import android.widget.Toolbar;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.annotation.NonNull;
@@ -12,18 +13,34 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    private DayOfWeekFragment dayOfWeekFragment;
+    private DayOfWeekFragment dayOfWeekFragment1;
+    private DayOfWeekFragment dayOfWeekFragment2;
+    private DayOfWeekFragment dayOfWeekFragment3;
+    private DayOfWeekFragment dayOfWeekFragment4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //sec version 2check
+        //start another activity only once after install
         Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
                 .getBoolean("isFirstRun", true);
 
@@ -34,8 +51,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     .putBoolean("isFirstRun", false).apply();
         }
 
-
-
+        //navigation
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -48,11 +64,37 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new MessageFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_message);
-        }
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                    new MessageFragment()).commit();
+//            navigationView.setCheckedItem(R.id.nav_message);
+//        }
+
+        //tablayout
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tab_layout);
+
+        dayOfWeekFragment = new DayOfWeekFragment();
+        dayOfWeekFragment1 = new DayOfWeekFragment();
+        dayOfWeekFragment2 = new DayOfWeekFragment();
+        dayOfWeekFragment3 = new DayOfWeekFragment();
+        dayOfWeekFragment4 = new DayOfWeekFragment();
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), 0);
+//        String[] name_tab ={"Monday", "Tuesday","Wednesday","Thursday","Friday"};
+//        for (int i=0; i<name_tab.length+1; i++){
+//            dayOfWeekFragment = new DayOfWeekFragment();
+//            viewPagerAdapter.addFragment(dayOfWeekFragment, name_tab[i]);
+//        }
+        viewPagerAdapter.addFragment(dayOfWeekFragment, "Monday");
+        viewPagerAdapter.addFragment(dayOfWeekFragment1, "Tuesday");
+        viewPagerAdapter.addFragment(dayOfWeekFragment2, "Wednesday");
+        viewPagerAdapter.addFragment(dayOfWeekFragment3, "Thursday");
+        viewPagerAdapter.addFragment(dayOfWeekFragment4, "Friday");
+        viewPager.setAdapter(viewPagerAdapter);
+
     }
 
     @Override
@@ -101,6 +143,39 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+        }
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragments = new ArrayList<>();
+        private List<String> fragmentTitle = new ArrayList<>();
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            fragments.add(fragment);
+            fragmentTitle.add(title);
+
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitle.get(position);
         }
     }
 }
